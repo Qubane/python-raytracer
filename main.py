eb9a2047a6a7e0f4e332d80d012f2cdd3b4b833f
UltraQbik <no1skill@yandex.ru>
2024-05-11 21:21:27 +0300

    Make window work

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from src import *
+from time import sleep
 
 
 def main():
-    win = Window()
+    win = Window(1280, 720)
+    while win.alive:
+        win.update()
+        sleep(0.033)
+    print(1)
 
 
 if __name__ == '__main__':
--- src/window.py
+++ src/window.py
@@ -6,5 +6,37 @@ class Window:
     Tkinter window class
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, width: int, height: int):
+        self.width: int = width
+        self.height: int = height
+
+        # tkinter part
+        self._tk_root = tk.Tk()
+        self._tk_root.title("Raytracer")
+        self._tk_root.protocol("WM_DELETE_WINDOW", lambda: self._die(self._tk_root))
+
+        self._tk_canvas = tk.Canvas(self._tk_root, width=self.width, height=self.height)
+        self._tk_canvas.pack(padx=0, pady=0, expand=True)
+        self._tk_image = tk.PhotoImage(width=self.width, height=self.height)
+        self._tk_canvas.create_image(self.width // 2, self.height // 2, image=self._tk_image, state='normal')
+
+        self._tk_alive: bool = True
+
+    def _die(self, win):
+        """
+        When window dies
+        """
+
+        self._tk_alive = False
+        win.destroy()
+
+    @property
+    def alive(self):
+        return self._tk_alive
+
+    def update(self):
+        """
+        Updates the window
+        """
+
+        self._tk_root.update()
